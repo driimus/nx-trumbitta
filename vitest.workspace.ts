a8cff5ac3d76a89f912f19ed5dc62c4f819b426b
Cristian Petre <driimus@vivaldi.net>
2024-04-24 23:29:11 +0100

    chore(setup): nx vitest integration

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,1 +1,13 @@
-export default ['**/*/vite.config.ts', '**/*/vitest.config.ts'];
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+import { defineWorkspace } from 'vitest/config';
+
+export default defineWorkspace([
+  'packages/**',
+  {
+    plugins: [nxViteTsPaths()],
+    test: {
+      globals: true,
+    },
+  },
+]);
